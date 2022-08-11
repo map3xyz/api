471692c1f536038503942701a1b2d2d73058f4db
Arshad Mahmood <arshadm@octcart.com>
2022-08-11 19:22:20 +0100

    added support for downloading the latest assetdb release

---
+++ app/util/assetdb.ts
@@ -0,0 +1,45 @@
+import "isomorphic-fetch";
+import { createWriteStream } from "node:fs";
+import { mkdir, open } from "node:fs/promises";
+import { Octokit } from "octokit";
+
+const ASSET_REPO_OWNER = "map3xyz";
+const ASSET_REPO_NAME = "assets";
+const ASSET_DB_DIR = "/tmp/assetdb";
+
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+getLatestRelease().then((res) => console.log(res));
+console.log("Done");
+
+async function getLatestRelease(): Promise<string> {
+  const release = await octokit.rest.repos.getLatestRelease({ owner: ASSET_REPO_OWNER, repo: ASSET_REPO_NAME });
+  const exists = await doesAssetDbExist(release.data.tag_name);
+
+  if (!exists) {
+    const file = await createWriteStream(ASSET_DB_DIR + "/" + release.data.tag_name);
+    const response = await fetch(release.data.assets[0].browser_download_url);
+    const body = await response.text();
+    file.write(body);
+    file.close();
+
+    const latest = await createWriteStream(ASSET_DB_DIR + "/LATEST_ID");
+    latest.write(release.data.tag_name);
+    latest.close();
+  }
+
+  return Promise.resolve(release.data.tag_name);
+}
+
+async function doesAssetDbExist(tagName: string): Promise<boolean> {
+  let dbExists = false;
+
+  try {
+    const dir = await mkdir(ASSET_DB_DIR);
+    const file = await open(ASSET_DB_DIR + "/" + tagName, "ro");
+    dbExists = true;
+  } catch (err: any) {}
+
+  return Promise.resolve(dbExists);
+}
+
