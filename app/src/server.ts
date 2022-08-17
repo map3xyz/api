6a82fe2882ad157fa79b1d12e96196a82cf99bcc
Arshad Mahmood <arshadm@octcart.com>
2022-08-17 16:58:47 +0100

    added jwt based authentication

--- app/src/server.ts
+++ app/src/server.ts
@@ -2,17 +2,19 @@ import dotenv from "dotenv";
 import express from "express";
 import { queryAssets } from "./api/asset";
 import { queryNetworks } from "./api/network";
+import { authenticateToken } from "./lib/auth";
 
 const app = express();
 const router = express.Router();
 
+dotenv.config({ path: ".env.local" });
 dotenv.config();
 
-router.get("/v1/network", async (req, res) => {
+router.get("/v1/network", authenticateToken, async (req, res) => {
   return queryNetworks(req, res);
 });
 
-router.get("/v1/asset", async (req, res) => {
+router.get("/v1/asset", authenticateToken, async (req, res) => {
   return queryAssets(req, res);
 });
 
