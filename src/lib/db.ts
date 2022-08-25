64b981d3652a87d6fc1d9510dc62c8da49236530
Amadeo Pellicce <pellicceama@users.noreply.github.com>
2022-08-25 17:29:06 +0200

    not reusing DB connection in case DB changes with cron process

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,27 +2,23 @@ import { readFile } from "node:fs/promises";
 import { PromisedDatabase } from "promised-sqlite3";
 import { ASSETDB_DIR } from "../util/config";
 
-let db: PromisedDatabase; 
-
 export async function getConnection(
   assetDbDir: string = ASSETDB_DIR
 ): Promise<PromisedDatabase> {
-  if(!db) {
-    const filename = await getDbLocation(assetDbDir);
+  const filename = await getDbLocation(assetDbDir);
 
-    db = new PromisedDatabase();
-    await db.open(filename);
+  const db = new PromisedDatabase();
+  await db.open(filename);
 
-    try {
-      const res = await db.get('SELECT network_code from network limit 1');
-      if (!res || res === '') {
-        console.log('No networks found in database', res)
-        throw new Error('No networks found in database');
-      }
-    } catch (err) {
-      console.log('Error fetching database. Database is empty. Please run yarn update to get the latest version')
-      throw err;
+  try {
+    const res = await db.get('SELECT network_code from network limit 1');
+    if (!res || res === '') {
+      console.log('No networks found in database', res)
+      throw new Error('No networks found in database');
     }
+  } catch (err) {
+    console.log('Error fetching database. Database is empty. Please run yarn update to get the latest version')
+    throw err;
   }
 
   return Promise.resolve(db);
@@ -36,4 +32,3 @@ export async function getDbLocation(assetDbDir: string = ASSETDB_DIR): Promise<s
 
   return Promise.resolve((latestDb));
 }
-
