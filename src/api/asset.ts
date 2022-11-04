7c32714adf44d5759cb9e00b7a7fae327254f90e
Amadeo Pellicce <pellicceama@users.noreply.github.com>
2022-11-04 11:30:49 +0100

    Adding maps to asset API

--- src/api/asset.ts
+++ src/api/asset.ts
@@ -24,11 +24,45 @@ export async function queryAssets(req: Request, res: Response): Promise<void> {
   }
 
   const asset = await db.get(query, params);
-  if (asset) {
-    res.status(200).json(JSON.parse(asset.asset_data));
-  } else {
+
+  if(!asset) {
     res.status(404).end();
   }
+
+  asset.asset_data = JSON.parse(asset.asset_data);
+
+  query = "SELECT * FROM asset_map WHERE to_network=$to_network AND to_address=$to_address";
+  params = { $to_network: asset.asset_data.networkCode, $to_address: asset.asset_data.address };
+
+  const maps = await db.all(query, params);
+
+  if(maps && maps.length > 0) {
+    const networkCodes = [...new Set(maps.map((map: any) => map.from_network))].sort();
+    const networks: {[networkCode: string]: string[]} = {};
+
+    for(const networkCode of networkCodes) {
+      const networkAddresses = maps
+        .filter((map: any) => map.from_network === networkCode)
+        .map((map: any) => map.from_address);
+
+      networks[networkCode + ''] = networkAddresses;
+    }
+    asset.asset_data.networks = networks;
+
+    const types = [...new Set(maps.map((map: any) => map.type))].sort();
+    const organizedMaps: {[type: string]: any[]} = {};
+
+    for(const type of types) {
+      const typeMaps = maps
+        .filter((map: any) => map.type === type)
+        .map((map: any) => JSON.parse(map.data));
+
+      organizedMaps[type + ''] = typeMaps;
+    }
+    asset.asset_data.maps = organizedMaps; 
+  }
+
+  res.status(200).json(asset.asset_data);
 
   return Promise.resolve();
 }
